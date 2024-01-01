9b63f67ad2c67a7fe8e3de31bc861d34ea0d306c
Shao-Wei Ho <scarlet8714@gmail.com>
2024-01-01 19:23:24 +0800

    'update'

--- pages/ProtectRoute.jsx
+++ pages/ProtectRoute.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function ProtectRoute({ children }) {
   const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  useEffect(function () {
+    alert("請先登入");
+    if (!isAuthenticated) navigate(-1);
+  });
   return isAuthenticated ? children : null;
 }
